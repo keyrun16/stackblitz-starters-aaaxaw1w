8ef5cc9be154a901c7ced99ec6f2bce928d31ac4
keyrun16 <keyrun16@gmail.com>
2025-11-27 23:12:50 +0530

    Initial commit

---
+++ src/Factorial.js
@@ -0,0 +1,14 @@
+function factorial(n) {
+  let result = 1;
+  for (i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(0)); //1
+console.log(factorial(1)); //1
+console.log(factorial(5)); //120
+
+// Time complexity -> Big O -> O(n)
+// node src/Factorial.js
---
+++ src/Fibanocci.js
@@ -0,0 +1,14 @@
+function fibanacci(n) {
+  const fib = [0, 1];
+  for (i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log('Hello world');
+console.log(fibanacci(4));
+console.log(fibanacci(6));
+console.log(fibanacci(9));
+
+// Time Complexity -> Big O -> O(n)
